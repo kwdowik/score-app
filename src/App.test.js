f5d12fe5cbf1cd27714fc76b2aa8b28d313936fb
Kacper Wdowik <wdowikkacper@gmail.com>
2020-10-20 07:26:52 +0200

    feat: add default template

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import 'regenerator-runtime/runtime.js';
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn xstate/i);
+  expect(linkElement).toBeInTheDocument();
+});
