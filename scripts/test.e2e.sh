2e8c8f2f1f6ece80a603b92b026026aaf5317d9c
Kacper Wdowik <wdowikkacper@gmail.com>
2020-11-08 12:08:39 +0100

    feat: add script to run e2e tests in fully automated way

---
+++ scripts/test.e2e.sh
@@ -0,0 +1,4 @@
+yarn docker:staging
+count=0; until docker logs score-api | grep "Server is listening" &> /dev/null || (( count++ > 12 )); do echo "Waiting $count"; sleep 5; done
+yarn test:e2e
+yarn docker:clear
