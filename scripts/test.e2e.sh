8732d6760b53335092a416fef6266ba651dd1a21
Kacper Wdowik <wdowikkacper@gmail.com>
2020-11-09 18:29:10 +0100

    chore: rename test's script

--- scripts/test.e2e.sh
+++ scripts/test.e2e.sh
@@ -1,4 +1,4 @@
 yarn docker:staging
 count=0; until docker logs score-api | grep "Server is listening" &> /dev/null || (( count++ > 12 )); do echo "Waiting $count"; sleep 5; done
-yarn test:e2e
+yarn test:e2e:in-container
 yarn docker:clear
